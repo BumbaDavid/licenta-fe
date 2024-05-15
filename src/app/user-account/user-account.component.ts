4d3e8fcc2c70f95240e70d8542fb40dfb98679f9
Bumba David <dave2000tm@yahoo.com>
2024-05-15 16:00:49 +0300

    added user account page

--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+export enum Tab {
+  PROFILE, CV, JOB_REQUESTS, CLOSE_ACCOUNT
+}
 @Component({
   selector: 'app-user-account',
   templateUrl: './user-account.component.html',
   styleUrls: ['./user-account.component.scss']
 })
 export class UserAccountComponent implements OnInit {
+  public tab = Tab;
 
+  openedTab: Tab | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    this.openedTab = Tab.PROFILE;
   }
 
+  changeTab(newTab: Tab) {
+      this.openedTab = newTab;
+  }
 }
